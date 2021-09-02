08088241d14aecc48f7c5f758d2c2b7e56b3d483
kazirishad <88087665+kazirishad@users.noreply.github.com>
2021-09-02 10:42:28 +0600

    Update AvailableRooms.java

--- HotelReservation-war/src/java/servlets/AvailableRooms.java
+++ HotelReservation-war/src/java/servlets/AvailableRooms.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "available_rooms")
+@WebServlet(name = "available_rooms0")
 public class AvailableRooms extends HttpServlet {
 
     @Override
